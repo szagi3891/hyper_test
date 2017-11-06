a0443dcc94e8967a844ee253a7697842de0b7c61
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-11-06 20:23:30 +0100

    serwowanie pliku statycznego

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@
 extern crate hyper;
 extern crate futures;
 extern crate tokio_core;
+extern crate futures_cpupool;
 
-use std::sync::mpsc::channel;
-use std::thread;
-use std::time::Duration;
+//use std::sync::mpsc::channel;
+//use std::thread;
+//use std::time::Duration;
 use std::net::SocketAddr;
 use futures::future::Future;
-
+use futures_cpupool::CpuPool;
 use hyper::Client;
 
 use hyper::{Method, StatusCode};
 use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service, Server};
+use hyper::server::{Http, Request, Response, Service};
 
 use futures::stream::Stream;
 
@@ -24,10 +25,14 @@ use tokio_core::reactor::Core;
 
 const PHRASE: &'static str = "Hellllllloooooo";
 
+mod file_read;
+//use file_read;
+
 //https://gist.github.com/meganehouser/d5e1b47eb2873797ebdc440b0ed482df
 
 struct HelloWorld {
-    tokio_handle: TokioHandle
+    tokio_handle: TokioHandle,
+    cpu_pool: CpuPool,
 }
 
 impl Service for HelloWorld {
@@ -37,6 +42,9 @@ impl Service for HelloWorld {
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
+
+        println!("Rwquest {}", req.path());
+
         match (req.method(), req.path()) {
 
             (&Method::Get, "/specjalny") => {
@@ -73,6 +81,19 @@ impl Service for HelloWorld {
                         })
                 )
             }
+            (&Method::Get, "/crash") => {
+                panic!("crash");
+            }
+
+            (&Method::Get, "/file") => {
+                let file_content = file_read::read_file("./src/main.rs");
+
+                println!("aaaa {}", file_content);
+
+                let mut response = Response::new();
+                response.set_body(file_content);
+                Box::new(futures::future::ok(response))
+            }
 
             _ => {
                 Box::new(futures::future::ok(
@@ -93,6 +114,8 @@ fn main() {
     
     println!("server start {}", addr);
 
+    let cpu_pool = CpuPool::new_num_cpus();
+
     let http = Http::new();
     let mut core = Core::new().unwrap();
     let handle = core.handle();
@@ -101,7 +124,12 @@ fn main() {
     let server = listener
         .incoming()
         .for_each(|(sock, addr)| {
-            let hello_world = HelloWorld { tokio_handle: handle.clone() };
+
+            let hello_world = HelloWorld {
+                tokio_handle: handle.clone(),
+                cpu_pool: cpu_pool.clone()
+            };
+
             http.bind_connection(&handle, sock, addr, hello_world);
             Ok(())
         });
